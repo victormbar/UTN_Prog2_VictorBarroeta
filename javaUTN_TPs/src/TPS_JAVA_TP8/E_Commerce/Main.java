f5ae090bbebdb78924eae5b9729cc5a88091376d
Victormanuel.bar <victormanuel.bar28@gmail.com>
2025-11-21 03:57:02 -0300

    TP 8

---
+++ javaUTN_TPs/src/TPS_JAVA_TP8/E_Commerce/Main.java
@@ -0,0 +1,11 @@
+
+package TPS_JAVA_TP8.E_Commerce;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
---
+++ javaUTN_TPs/src/TPS_JAVA_TP8/Excepciones/Main.java
@@ -0,0 +1,10 @@
+
+package TPS_JAVA_TP8.Excepciones;
+
+public class Main {
+
+    public static void main(String[] args) {
+  
+    }
+    
+}
